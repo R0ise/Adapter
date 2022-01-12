0dfa5d65fe05faa20a4bc45cbd0e34fe3ec2c06d
R0ise <the.roise.511@gmail.com>
2022-01-12 16:27:57 +0400

    Adapter

---
+++ Adapter/Adapter/Adapter/Boat.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adapter
+{
+    class Boat : ISeaworthy
+    {
+        public void Swim()
+        {
+            Console.WriteLine("Лодка плывет по воде");
+        }
+    }
+}
---
+++ Adapter/Adapter/Adapter/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Adapter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // путешественник
+            Driver driver = new Driver();
+            // машина
+            Auto auto = new Auto();
+            // отправляемся в путешествие
+            driver.Travel(auto);
+            // Впереди вода, придется плыть на лодке
+            Boat boat = new Boat();
+            // используем адаптер
+            ITransport BoatTransport = new BoatToTransportAdapter(boat);
+            // продолжаем плыть по воде
+            driver.Travel(BoatTransport);
+
+            Console.Read();
+        }
+    }
+}
